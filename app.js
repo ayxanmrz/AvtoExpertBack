39270ee9653d66125ae50dbd857fb4c828f382c7
ayxan-mirzeyev-7674 <64847501+ayxan-mirzeyev-7674@users.noreply.github.com>
2025-03-05 22:21:52 +0400

    Getting currensies from api

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import puppeteer from "puppeteer";
 import express from "express";
 import cors from "cors";
 import NodeCache from "node-cache";
+import axios from "axios";
 
 const PORT = 4000;
 const app = express();
 const cache = new NodeCache({ stdTTL: 300 });
+
+let EURO_AZN = 1.8;
+let USD_AZN = 1.7;
 
 app.use(cors());
 
@@ -34,7 +38,7 @@ async function getRandomCars() {
   const page = await getPage();
   try {
     await page.goto(
-      "https://turbo.az/autos?pages=" + (Math.floor(Math.random() * 5) + 1),
+      "https://turbo.az/autos?pages=" + (Math.floor(Math.random() * 20) + 1),
       {
         waitUntil: "domcontentloaded",
       }
@@ -44,12 +48,11 @@ async function getRandomCars() {
       const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
       const sampleSize = (arr, n = 1) => shuffle(arr).slice(0, n);
 
-      return sampleSize(
-        [...document.querySelectorAll(".products-i")].map(
-          (elem) => elem.querySelector(".products-i__link")?.href
-        ),
-        10
+      const carDatas = [...document.querySelectorAll(".products-i")].map(
+        (elem) => elem.querySelector(".products-i__link")?.href
       );
+
+      return sampleSize(carDatas, Math.min(carDatas.length, 20));
     });
   } catch (error) {
     console.error("Error fetching random cars:", error);
@@ -64,59 +67,91 @@ async function getCarInfo(carUrl) {
   try {
     await page.goto(carUrl, { waitUntil: "domcontentloaded" });
 
-    return await page.evaluate(() => {
-      const getManatPrice = (string) => {
-        const stringList = string.split(" ");
-        const currency = stringList[stringList.length - 1];
-        const value = stringList
-          .slice(0, stringList.length - 1)
-          .reduce((res, elem) => res + elem);
-        if (currency === "AZN") {
-          return +value;
-        } else if (currency === "USD") {
-          return value * 1.7;
-        } else {
-          return null;
-        }
-      };
+    return await page.evaluate(
+      (USD_AZN, EURO_AZN) => {
+        const getManatPrice = (string) => {
+          const stringList = string.split(" ");
+          const currency = stringList[stringList.length - 1];
+          const value = stringList
+            .slice(0, stringList.length - 1)
+            .reduce((res, elem) => res + elem);
+          if (currency === "AZN") {
+            return +value;
+          } else if (currency === "USD") {
+            return Math.round(value * (USD_AZN || 1.7));
+          } else if (currency === "EUR") {
+            return Math.round(value * (EURO_AZN || 1.8));
+          } else {
+            return null;
+          }
+        };
 
-      return {
-        title:
-          document
-            .querySelector(".product-title")
-            ?.textContent.split(", ")[0] || "Unknown",
-        year:
-          document.querySelector(
-            ".product-properties__i-name[for='ad_reg_year']+span a"
-          )?.textContent || "Unknown",
-        mileage:
-          document
-            .querySelector(".product-properties__i-name[for='ad_mileage']")
-            ?.nextSibling?.textContent.trim() || "Unknown",
-        engine:
-          document
-            .querySelector(
-              ".product-properties__i-name[for='ad_engine_volume']"
-            )
-            ?.nextSibling?.textContent.trim() || "Unknown",
-        transmission:
-          document
-            .querySelector(".product-properties__i-name[for='ad_transmission']")
-            ?.nextSibling?.textContent.trim() || "Unknown",
-        images: [
-          ...document.querySelectorAll(".slick-slide:not(.slick-cloned) img"),
-        ].map((elem) => elem.src),
-        price:
-          getManatPrice(
-            document.querySelector(".product-price__i")?.textContent
-          ) || "Unknown",
-      };
-    });
+        return {
+          title:
+            document
+              .querySelector(".product-title")
+              ?.textContent.split(", ")[0] || "Unknown",
+          year:
+            document.querySelector(
+              ".product-properties__i-name[for='ad_reg_year']+span a"
+            )?.textContent || "Unknown",
+          mileage:
+            document
+              .querySelector(".product-properties__i-name[for='ad_mileage']")
+              ?.nextSibling?.textContent.trim() || "Unknown",
+          engine:
+            document
+              .querySelector(
+                ".product-properties__i-name[for='ad_engine_volume']"
+              )
+              ?.nextSibling?.textContent.trim() || "Unknown",
+          transmission:
+            document
+              .querySelector(
+                ".product-properties__i-name[for='ad_transmission']"
+              )
+              ?.nextSibling?.textContent.trim() || "Unknown",
+          images: [
+            ...document.querySelectorAll(".slick-slide:not(.slick-cloned) img"),
+          ].map((elem) => elem.src),
+          price:
+            getManatPrice(
+              document.querySelector(".product-price__i")?.textContent
+            ) || "Unknown",
+        };
+      },
+      USD_AZN,
+      EURO_AZN
+    );
   } catch (error) {
     console.error("Error fetching car info:", error);
     return { error: "Failed to fetch car details" };
   } finally {
     await page.close();
+  }
+}
+
+async function getEuroConverts() {
+  try {
+    const response = await axios.get(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+    );
+    EURO_AZN = response.data.eur.azn;
+    console.log("EURO_AZN:", EURO_AZN);
+  } catch (error) {
+    console.error("Error fetching the data:", error);
+  }
+}
+
+async function getUsdConverts() {
+  try {
+    const response = await axios.get(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
+    );
+    USD_AZN = response.data.usd.azn;
+    console.log("USD_AZN:", USD_AZN);
+  } catch (error) {
+    console.error("Error fetching the data:", error);
   }
 }
 
@@ -140,6 +175,10 @@ app.get("/get-random-cars", async (req, res) => {
 
 app.listen(PORT, async () => {
   await launchBrowser();
+  await getEuroConverts();
+  await getUsdConverts();
+  console.log(EURO_AZN);
+  console.log(USD_AZN);
   console.log(
     `[SERVER] ExpressJS is listening to port http://localhost:${PORT}`
   );
@@ -150,7 +189,7 @@ process.on("exit", async () => {
 });
 
 // const getManatPrice = (string) => {
-//   const stringList = string.split(" ");
+//   const stringList = string.spl it(" ");
 //   const currency = stringList[stringList - 1];
 //   const value = stringList
 //     .slice(0, stringList.length - 1)
