f9cd19a331c300bd59714ea334eaf5e7e1845e57
ayxanmrz <amirzeyev3@gmail.com>
2025-09-13 21:18:13 +0400

    Deploy file changed for handling ip

--- deploy.sh
+++ deploy.sh
@@ -83,6 +83,9 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "Upgrade";
         proxy_set_header Host \$host;
+        # Pass the real client IP to the backend
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 }
 EOL
